cbd14910eddc287e3d355ed59a1601e5cc6828b5
Justin Duong <jduong@umich.edu>
2022-11-02 20:50:08 -0400

    First commit, laid out html and added GET request to grab all countries from REST Countries API.

---
+++ js/countries.js
@@ -0,0 +1,52 @@
+const populateCountriesList = (countries, listCountries) => {
+  countries.forEach(country => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('item-country');
+    
+    const imgCountry = document.createElement('img');
+    imgCountry.src = country.flags.png;
+    imgCountry.classList.add('img-flag');
+    imgCountry.alt = `flag of ${country.name.official}`;
+
+    const name = document.createElement('h2');
+    name.innerText = country.name.official;
+
+    const information = document.createElement('div');
+    information.classList.add('div-information');
+
+    const population = document.createElement('div');
+    population.innerHTML = `<span>Population</span>${country.population}`;
+
+    const region = document.createElement('div');
+    region.innerHTML = `<span>Region</span>${country.region}`;
+
+    const capital = document.createElement('div');
+    capital.innerHTML = `<span>Capital</span>${country.capital}`;
+
+    information.append(population, region, capital);
+
+    listItem.append(imgCountry, name, information);
+
+    listCountries.append(listItem);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const listCountries = document.querySelector('.list-countries');
+
+  const http = new XMLHttpRequest();
+
+  http.onload = () => {
+    console.log(http.status);
+    if (http.status === 200 && http.readyState === 4) {
+      const json = JSON.parse(http.response);
+      console.log(json);
+      populateCountriesList(json, listCountries);
+    } else {
+      console.log(http.responseText);
+    }
+  }
+
+  http.open('GET', 'https://restcountries.com/v3.1/all');
+  http.send();
+});
